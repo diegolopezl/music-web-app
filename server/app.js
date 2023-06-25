a14f2e54af23ee561b94dff82cae620ef451e4c9
Diego Lopez <diegolopezlatour@gmail.com>
2023-06-25 17:16:17 -0400

    app.js file change/start script

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 //Dependencias y librerias
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
 
 //Render de los files
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('../music-app'));
 app.use(express.json());
 
-
-
-app.listen(port , () => {
-    console.log(`Server listening at port ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
 });
