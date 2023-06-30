52a6debb17de7ac2816e6415788878687664b4d6
Diego Lopez <diegolopezlatour@gmail.com>
2023-06-30 16:12:48 -0400

    login component

---
+++ music-app/src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import arrow from "../assets/arrow-hero.svg";
+import blobs from "../assets/blobs.svg";
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=CLIENT_ID&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return (
+    <section className="login-section">
+      <div className="login-box">
+        <>
+          <h1>Hello, Welcome!</h1>
+          <p>Enjoy the music and customize your experience!</p>
+          <img src={arrow} />
+        </>
+        <a className="log-in-spotify" href={AUTH_URL}>
+          Login with Spotify
+        </a>
+      </div>
+      <img className="color-blobs" src={blobs} />
+    </section>
+  );
+}
