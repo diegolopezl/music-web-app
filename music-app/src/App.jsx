ae5b951eef72d71e20939ac17f2b6faf1ae10d67
Near <ogmelgen@gmail.com>
2023-07-01 19:50:22 -0400

    xd

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-//import "./App.css";
+import "./App.css";
 
 import Login from "./components/Login";
-import Test from "./components/Test";
 export default function App() {
   const [count, setCount] = useState(0);
 
-  return <Test />;
+  return <Login />;
 }
--- server/app.js
+++ server/app.js
@@ -2,15 +2,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 
+//Middlewares para paso de datos entre front end y back end
+app.use(express.json());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
-
-app.get("/test", (req,res) => {
-  res.json({ message: "Received your request successfully!" });
-});
 
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
