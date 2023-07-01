47e0dfdaae35d78c6f7f2659010a60513c7486b8
Near <ogmelgen@gmail.com>
2023-07-01 19:56:54 -0400

    Backend fix

--- server/app.js
+++ server/app.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 
-//Render de los files
-app.use(express.urlencoded({ extended: true }));
+//Middlewares para paso de datos entre front end y back end
 app.use(express.json());
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
