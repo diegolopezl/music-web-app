4a652c5d70d5f165cc2dcf4966c06d4eed427520
Near <ogmelgen@gmail.com>
2023-07-01 20:14:06 -0400

    Authentication for spotify

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
+const SpotifyWebAPI = require('spotify-web-api-node');
 
 //Middlewares para paso de datos entre front end y back end
 app.use(express.json());
@@ -10,6 +11,56 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken
+  const spotifyApi = new SpotifyWebAPI({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+
+//Autenticacion del token del login
+app.post('/login' , (req,res) => {
+  const code = req.body.code;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  spotifyApi
+  .authorizationCodeGrant(code)
+  .then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
+    })
+  })
+  .catch(err => {
+    res.sendStatus(400)
+  })
+
+});
 
 
 app.listen(port, () => {
